061ea68f407b75277fcd19d54c6f15dccd67a182
Brandon Velásquez <brandon.velasquez.osorio@gmail.com>
2022-04-22 00:00:16 -0500

    version 0.1

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const http = require('http');
-const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 
-const io = socketIO(server);
+//Inicializamos socketio
+const socketIO = require('socket.io');
+const io = socketIO(server, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});;
+
+//Funcionalidad de socket.io en el servidor
 io.on('connection', (socket) => {
-  socket.on('connected', (msg) => {
-    console.log("connected");
+  socket.on('connected', (name) => {
+    let _name = name;
+    //socket.broadcast.emit manda el mensaje a todos los clientes excepto al que ha enviado el mensaje
+    socket.broadcast.emit('messages', { name: _name, message: `${_name} se ha conectado` });
+    console.log(`${name} connected`);
+  });
+
+  socket.on('message', (name, message) => {
+    //io.emit manda el mensaje a todos los clientes conectados al chat
+    io.emit('message', { name, message });
+    console.log(`message from ${name}: ${message}`);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('messages', { server: 'Server', message: 'User disconnected' });
+    console.log('user disconnected');
   });
   console.log('New user connected');
 });
 
 
-server.listen(3000, () => {
-    console.log('listening on localhost:3000');
+server.listen(3001, () => {
+  console.log('listening on localhost:3001');
 });
